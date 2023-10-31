a4a7e7421242287d65430261d5680ec5c5bd8bd0
SashkaHacker <am942081@gmail.com>
2023-10-31 21:58:55 +0300

    add individual.py

--- individual.py
+++ individual.py
@@ -11,18 +11,18 @@ EULER = 0.5772156649015328606
 # Точность вычислений.
 EPS = 1e-10
 
-if __name__ == "__main__":
-    x = float(input("Введите значение x: "))
+if __name__ == '__main__':
+    x = float(input("Value of x? "))
     if x == 0:
         print("Illegal value of x", file=sys.stderr)
         exit(1)
 
-    n = 1
-    term = (-1) ** n * x ** (2 * n) / (math.factorial(2 * n) * (2 * n))
-    s = term
-    while abs(term) > EPS:
-        n += 1
-        term = (-1) ** n * x ** (2 * n) / (math.factorial(2 * n) * (2 * n))
-        s += term
+    a = -(x ** 4) / 4
+    S, n = a, 1
 
-    print(f"Ci({x}) = {EULER + math.log(abs(x)) + s}")
+    while math.fabs(a) > EPS:
+        a *= -(x ** 2 * n) / (4 * n ** 3 + 10 * n ** 2 + 8 * n + 2)
+        S += a
+        n += 1
+
+    print(f"Ci({x}) = {EULER + math.log(math.fabs(x)) + S}")
